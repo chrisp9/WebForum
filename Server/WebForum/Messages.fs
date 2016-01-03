a7ed3775dbf8685ea66ed4fa579d731c4c2ade37
chrisp9 <chris_powell9@outlook.com>
2016-01-03 21:54:34 +0000

    Added messages for creating users, authenticating and adding a post

---
+++ Server/WebForum/Messages.fs
@@ -0,0 +1,23 @@
+﻿namespace WebForum.WebServer.HttpApi
+
+open System
+
+[<CLIMutable>]
+type AddUserMessage = {
+   Username : string
+   Password : string
+   Email : string
+}
+
+[<CLIMutable>]
+type AuthenticateMessage = {
+   Username : string
+   Password : string
+}
+
+[<CLIMutable>]
+type AddPostMessage = {
+   Title : string
+   Message : string
+   Username : string
+}
